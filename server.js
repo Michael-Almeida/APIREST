aa46cd4c69d4cc9f8deee0bcbe80797645aff35d
Michael Almeida <michael.almeida@fundatec.org.br>
2024-02-02 11:06:01 -0300

    CRUD com dados locais

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
+const PORT = 3000;
 
-const PORT =3000
-
-const rotas ={
-    "/":"Curso de API Node.js",
-    "/livros":"Entrei na rota livros",
-    "/autores":"Entrei na rota autores"
-}
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end(rotas[req.url]);
+app.listen(PORT, () => {
+  console.log("Servidor escutando");
 });
-
-server.listen(PORT,()=>{
-    console.log("Servidor escutando");
-})
